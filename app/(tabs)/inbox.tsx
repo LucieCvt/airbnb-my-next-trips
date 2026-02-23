990600c878f89a515f286981b8bc8d21baa45359
Lucie Chauvet <lchauvet1094@gmail.com>
2026-02-23 18:08:58 +0100

    feat(nav): setup navigation

---
+++ app/(tabs)/inbox.tsx
@@ -0,0 +1,7 @@
+import { View } from "tamagui";
+
+const InboxScreen = () => {
+  return <View></View>;
+};
+
+export default InboxScreen;
---
+++ app/(tabs)/wishlists.tsx
@@ -0,0 +1,7 @@
+import { View } from "tamagui";
+
+const WishlistsScreen = () => {
+  return <View></View>;
+};
+
+export default WishlistsScreen;
