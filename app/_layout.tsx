ff37c066f658879b9237d432705a36b1913837d3
Lucie Chauvet <lchauvet1094@gmail.com>
2026-02-23 20:21:27 +0100

    feat(ui): use Expo useFont hook to handle new fonts and load SplashScreen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,47 @@
+import { useEffect } from "react";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import * as SplashScreen from "expo-splash-screen";
 import "react-native-reanimated";
-import { createTamagui, TamaguiProvider } from "@tamagui/core";
-import { defaultConfig } from "@tamagui/config/v5";
+import { TamaguiProvider } from "@tamagui/core";
+import {
+  useFonts,
+  Montserrat_400Regular,
+  Montserrat_500Medium,
+  Montserrat_600SemiBold,
+  Montserrat_700Bold,
+} from "@expo-google-fonts/montserrat";
+import config from "../core/tamagui.config";
+import "../core/i18n";
 
-const config = createTamagui(defaultConfig);
+SplashScreen.preventAutoHideAsync();
 
 export const unstable_settings = {
   anchor: "(tabs)",
 };
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    Montserrat: Montserrat_400Regular,
+    MontserratMedium: Montserrat_500Medium,
+    MontserratSemiBold: Montserrat_600SemiBold,
+    MontserratBold: Montserrat_700Bold,
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <TamaguiProvider config={config} defaultTheme="dark">
+    <TamaguiProvider config={config} defaultTheme="light">
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen
-          name="modal"
-          options={{ presentation: "modal", title: "Modal" }}
-        />
       </Stack>
       <StatusBar style="auto" />
     </TamaguiProvider>
